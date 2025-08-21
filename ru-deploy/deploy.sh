8d59938faaa65cc5f56e8b28cb36550f5f9019d2
LizardKing <lizardking131313@gmail.com>
2025-08-22 00:48:17 +0300

    RADIO-39 - Blocks

--- ru-deploy/deploy.sh
+++ ru-deploy/deploy.sh
@@ -106,7 +106,19 @@ server {
 
     # Главный прокси
     location / {
-        proxy_pass UPSTREAM_SCHEME_PLACEHOLDER://UPSTREAM_HOST_PLACEHOLDER;
+        proxy_pass https://radio.govnovoz-fm.fun;
+        proxy_set_header Host radio.govnovoz-fm.fun;
+
+        proxy_ssl_server_name on;
+        proxy_ssl_name radio.govnovoz-fm.fun;
+
+        proxy_http_version 1.1;
+        proxy_set_header X-Real-IP $remote_addr;
+        proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
+        proxy_set_header X-Forwarded-Proto https;
+
+        proxy_buffering off;
+        proxy_read_timeout 600s;
     }
 
     # Простой healthcheck
